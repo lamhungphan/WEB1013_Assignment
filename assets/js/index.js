1bd46df06a1909b1a931283f4f4f54928c37973b
Phan Lam Hung <lamhungphan1999@gmail.com>
2024-06-09 22:10:23 +0700

    demo to github pages

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+// login
+function showLogin() {
+    document.getElementById('loginModal').style.display = 'block';
+}
+
+function closeLogin() {
+    document.getElementById('loginModal').style.display = 'none';
+}
+
+function showRegister() {
+    document.getElementById('registerModal').style.display = 'block';
+}
+
+function closeRegister() {
+    document.getElementById('registerModal').style.display = 'none';
+}
